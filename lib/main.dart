69448b3c35b4787834f8e5d82bf40b0b819eaf3d
ashish kumar <ak3616272@gmail.com>
2021-09-04 16:45:43 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:io';
-import 'dart:async';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,76 +10,110 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Create Folder",
+      title: "Hero UI Design",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.pink),
-      home: CreateFolder(),
+      home: HeroUi(),
     );
   }
 }
 
-class CreateFolder extends StatefulWidget {
-  const CreateFolder({Key? key}) : super(key: key);
+class HeroUi extends StatefulWidget {
+  const HeroUi({Key? key}) : super(key: key);
 
   @override
-  _CreateFolderState createState() => _CreateFolderState();
+  _HeroUiState createState() => _HeroUiState();
 }
 
-class _CreateFolderState extends State<CreateFolder> {
-  TextEditingController _controller = TextEditingController();
-  @override
-  void initState() {
-    super.initState();
-    getpermission();
-  }
-
-  Future getpermission() async {
-    await Permission.manageExternalStorage.request().isGranted;
-    await Permission.storage.request().isGranted;
-  }
-
-  Future Folder(String data) async {
-    Directory dir =
-        await Directory("/storage/emulated/0/$data").create(recursive: false);
-    print(dir);
-  }
-
+class _HeroUiState extends State<HeroUi> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.dark,
-        statusBarColor: Colors.transparent));
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        centerTitle: true,
         backgroundColor: Colors.transparent,
-        title: Text("Create Folder"),
+        centerTitle: true,
+        title: Text("Hero UI Design"),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (var i = 0; i < 6; i++)
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                            transitionDuration: Duration(milliseconds: 500),
+                            pageBuilder: (BuildContext context,
+                                    Animation<double> animation,
+                                    Animation<double> secondaryanimation) =>
+                                Second_Screen(
+                                  data: "$i",
+                                ),
+                            transitionsBuilder: (BuildContext context,
+                                Animation<double> animation,
+                                Animation<double> secondaryAnimation,
+                                Widget child) {
+                              return Align(
+                                child: FadeTransition(
+                                  opacity: animation,
+                                  child: child,
+                                ),
+                              );
+                            }));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(20),
+                    height: 200,
+                    width: 200,
+                    child: Hero(
+                      tag: "$i",
+                      child: CircleAvatar(
+                        backgroundColor: Colors.pink,
+                      ),
+                    ),
+                  ),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Second_Screen extends StatefulWidget {
+  final data;
+  const Second_Screen({Key? key, @required this.data}) : super(key: key);
+
+  @override
+  _Second_ScreenState createState() => _Second_ScreenState();
+}
+
+class _Second_ScreenState extends State<Second_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text("Second screen"),
       ),
       body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(
-              controller: _controller,
-              decoration: InputDecoration(hintText: "Enter Folder Name"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  if (await Permission.manageExternalStorage
-                      .request()
-                      .isGranted) {
-                    if (_controller.text.isNotEmpty) {
-                      Folder(_controller.text);
-                    } else {
-                      print("Enter folder name");
-                    }
-                  }
-                },
-                child: Text("Create"))
+            Hero(
+              tag: widget.data,
+              child: Container(
+                color: Colors.pink,
+                margin: EdgeInsets.all(15),
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width,
+              ),
+            )
           ],
         ),
       ),
